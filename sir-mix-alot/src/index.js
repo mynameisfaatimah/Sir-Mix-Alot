61c2b818a42aaae17f590c3517c8bed327837f0d
Faatimah Cleveland <faatimahcleveland@gmail.com>
2023-02-25 13:07:16 -0500

    initial commit

---
+++ sir-mix-alot/src/index.js
@@ -0,0 +1,38 @@
+import {BrowserRouter} from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <React.StrictMode>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// how to hide API key https://www.google.com/search?q=how+to+hide+api+key+in+react&sxsrf=AJOqlzXilFgBO8q22J451BhMVl7ROYfXSQ%3A1677272060131&ei=_CP5Y9HWB4LS5NoP36CXKA&oq=how+to+add+api+key+in+react+js&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQARgAMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADMgoIABBHENYEELADSgQIQRgAUABYAGCXHmgBcAF4AIABAIgBAJIBAJgBAMgBCMABAQ&sclient=gws-wiz-serp#kpvalbx=_HiX5Y9uwBcylqtsP5-ODsAI_32
+
+
